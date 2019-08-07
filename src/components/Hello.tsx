b6539c4e5f8127795dc3873d351db4cc37d51342
Henry <henry.chou@tuta.io>
2019-08-07 20:54:45 +0800

    [Fix] fix error caused by removed image

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-const img = require('../images/photo-1557236516-f8d2e4ff66f2.jpeg');
 
 const Hello = () => {
   const [num, setNum] = React.useState(0);
   return (
     <React.Fragment>
-      <img src={img} />
+      <h1>React & TypeScript Template</h1>
       <h1>Current Number: {num}</h1>
       <button onClick={() => setNum(num + 1)}>increment</button>
     </React.Fragment>
