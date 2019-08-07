de54854b91dde2e88c9d41e8dae34edfdc287c7a
Henry <henry.chou@tuta.io>
2019-08-07 20:55:11 +0800

    make the Hello component more concise

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 
 const Hello = () => {
-  const [num, setNum] = React.useState(0);
   return (
-    <React.Fragment>
+    <>
       <h1>React & TypeScript Template</h1>
-      <h1>Current Number: {num}</h1>
-      <button onClick={() => setNum(num + 1)}>increment</button>
-    </React.Fragment>
+      <div>Read README to learn how to use this template.</div>
+    </>
   );
 };
 
